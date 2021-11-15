af480381e0f1c0d845589fd512941d707bb3d9ac
Nightfallen <93279017+Nightfallen@users.noreply.github.com>
2021-11-15 17:02:31 +0300

    Hotfix for "Dashed Ranges" (#4)

--- EquiLand/source/custom_widgets.hpp
+++ EquiLand/source/custom_widgets.hpp
@@ -331,6 +331,141 @@ namespace widgets::EquiLand {
 		return result;
 	}
 
+	enum CARD_RANK {
+		Ace = 13,
+		King = 12,
+		Queen = 11,
+		Jack = 10,
+		TEN = 9,
+		NINE = 8,
+		EIGHT = 7,
+		SEVEN = 6,
+		SIX = 5,
+		FIVE = 4,
+		FOUR = 3,
+		THREE = 2,
+		TWO = 1,
+	};
+
+	int CardToEnum(char card)
+	{
+		switch (card)
+		{
+		case 'A': return CARD_RANK::Ace;
+		case 'K': return CARD_RANK::King;
+		case 'Q': return CARD_RANK::Queen;
+		case 'J': return CARD_RANK::Jack;
+		case 'T': return CARD_RANK::TEN;
+		case '9': return CARD_RANK::NINE;
+		case '8': return CARD_RANK::EIGHT;
+		case '7': return CARD_RANK::SEVEN;
+		case '6': return CARD_RANK::SIX;
+		case '5': return CARD_RANK::FIVE;
+		case '4': return CARD_RANK::FOUR;
+		case '3': return CARD_RANK::THREE;
+		case '2': return CARD_RANK::TWO;
+		default:
+			break;
+		}
+	}
+
+	char EnumToCard(int rank)
+	{
+		switch (rank)
+		{
+		case Ace: return 'A';
+			break;
+		case King:return 'K';
+			break;
+		case Queen:return 'Q';
+			break;
+		case Jack:return 'J';
+			break;
+		case TEN:return 'T';
+			break;
+		case NINE:return '9';
+			break;
+		case EIGHT:return '8';
+			break;
+		case SEVEN:return '7';
+			break;
+		case SIX:return '6';
+			break;
+		case FIVE:return '5';
+			break;
+		case FOUR:return '4';
+			break;
+		case THREE:return '3';
+			break;
+		case TWO:return '2';
+			break;
+		default:
+			break;
+		}
+	}
+
+	std::string RebuildDashedRange(const std::string& range)
+	{
+		std::string result;
+		auto dashPos = range.find("-");
+		auto posSuit = range.find("o") & range.find("s");
+		bool isPairRange = (posSuit == std::string::npos) ? true : false;
+		const char* separator = "";
+		if (isPairRange)
+		{
+			auto first_card = range[0];
+			auto card1 = CardToEnum(range[1]);
+			auto card2 = CardToEnum(range[range.size() - 1]);
+
+			for (auto card = card1; card >= card2; --card)
+			{
+				result.append(std::format("{}{}{}", separator, first_card, EnumToCard(card)));
+				separator = ",";
+			}
+		}
+		else
+		{
+			auto first_card = range[0];
+			auto suit = range[posSuit];
+			auto card1 = CardToEnum(range[1]);
+			auto card2 = CardToEnum(range[range.size() - 1 - 1]);
+
+			for (auto card = card1; card >= card2; --card)
+			{
+				result.append(std::format("{}{}{}{}", separator, first_card, EnumToCard(card), suit));
+				separator = ",";
+			}
+			
+		}
+		return result;
+	}
+
+	std::string RebuildRangeString(const std::string& def_range)
+	{
+		std::string range = def_range;
+		std::string result;
+		std::string delimiter = ",";
+
+		size_t pos = 0;
+		std::string token;
+		const char* separator = "";
+		while ((pos = range.find(delimiter)) != std::string::npos) {
+			token = range.substr(0, pos);
+
+			if (token.find("-") != std::string::npos)
+				token = RebuildDashedRange(token);
+
+			result.append(separator).append(token);
+			range.erase(0, pos + delimiter.length());
+			separator = ",";
+		}
+		if (pos == std::string::npos && result.empty() && range.find("-") != std::string::npos)
+			result = RebuildDashedRange(range);
+
+		std::cout << range << std::endl;
+		return result;
+	}
+
 	struct CardMatrix {
 		bool cards[169] = {};
 		std::string range;
@@ -777,7 +912,8 @@ namespace widgets::EquiLand {
 			using namespace omp;
 			auto hero_hand = BuildBoardString(cards);
 			auto board_cards = BuildBoardString(board);
-			std::vector<CardRange> ranges2{ range, hero_hand };
+			auto rebuilt_range = RebuildRangeString(range);
+			std::vector<CardRange> ranges2{ rebuilt_range, hero_hand };
 			omp::EquityCalculator equity;
 			uint64_t board_mask = CardRange::getCardMask(board_cards);
 			uint64_t dead_cards = CardRange::getCardMask(hero_hand);
